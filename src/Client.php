5e62fe01c97caa1e419864104bd8fe7d2cf1fdd3
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2017-06-17 23:07:02 -0400

    deal with this later, been heads down too long and can no longer think

--- src/Client.php
+++ src/Client.php
@@ -31,5 +31,31 @@ class Client implements ClientInterface
      */
     public function send()
     {
+        if (null === $this->request) {
+            throw new \RuntimeException('No Request to Send');
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->request->getUri());
+        curl_setopt($ch, CURLOPT_HEADER, true); // Include Headers with output
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // curl_exec returns output of request
+
+        if ('post' == strtolower($this->request->getMethod())) {
+            curl_setopt($ch, CURLOPT_POST, true);
+        }
+
+        $headers = array();
+        foreach ($this->request->getHeaders() as $name => $values) {
+            $headers[] = $name.': '.implode(', ', $values);
+        }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $rawResponse = curl_exec($ch);
+        curl_close($ch);
+
+        $lines = explode("\n", $rawResponse);
+        $status = null;
+        foreach ($lines as $line) {
+        }
     }
 }
