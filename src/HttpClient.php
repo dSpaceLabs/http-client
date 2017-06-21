b3b90e1c81b3d659a37f4fc37c60376bac459475
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2017-06-20 20:41:24 -0400

    thinking of just making this some generic soap and http client library

---
+++ src/HttpClient.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dspacelabs\Component\Http\Client;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Dspacelabs\Component\Http\Message\Response;
+use Dspacelabs\Component\Http\Message\Stream;
+
+/**
+ * Simple HTTP Client
+ */
+class HttpClient extends Client
+{
+}
